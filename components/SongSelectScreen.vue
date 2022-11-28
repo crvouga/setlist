43e817be31bf553377c73956608e7aad7ae5be88
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-11-28 16:04:55 -0700

    more consist ui

--- components/SongSelectScreen.vue
+++ components/SongSelectScreen.vue
@@ -15,37 +15,40 @@ const { data, pending } = useFetch("/api/song", {
 
 <template>
   <main class="container mt-2">
-    <div class="row">
-      <div class="col-12 d-flex justify-content-between align-items-center">
-        <h1 class="m-0">Add Song</h1>
-        <Button @click="onCreate">
-          <Icon name="plus" />
-          Create Song
-        </Button>
-      </div>
-    </div>
+    <section class="row justify-content-center">
+      <div class="col-12 col-md-8 col-lg-6">
+        <div class="d-flex justify-content-between align-items-center">
+          <h1 class="m-0">Select Song</h1>
+          <Button @click="onCreate">
+            <Icon name="plus" />
+            Create Song
+          </Button>
+        </div>
 
-    <div class="row mt-3">
-      <div class="col-12 d-flex gap-2 mb-2">
-        <input placeholder="Search songs" class="form-control" v-model="name" />
-        <Button :loading="pending">
-          <Icon name="search" />
-          Search
-        </Button>
-      </div>
+        <div class="d-flex gap-2 mb-2 mt-3">
+          <input
+            placeholder="Search songs"
+            class="form-control"
+            v-model="name" />
+          <Button :loading="pending">
+            <Icon name="search" />
+            Search
+          </Button>
+        </div>
 
-      <div
-        v-if="data?.type === 'Ok'"
-        class="col-12 px-4 d-flex flex-column mt-2 gap-4">
         <div
-          v-for="song in data.data"
-          v-bind:key="song.songId"
-          @click="onSelect({ songId: song.songId })">
-          <h4 class="m-0">
-            {{ song.songName }}
-          </h4>
+          v-if="data?.type === 'Ok'"
+          class="px-4 d-flex flex-column mt-2 gap-4">
+          <div
+            v-for="song in data.data"
+            v-bind:key="song.songId"
+            @click="onSelect({ songId: song.songId })">
+            <h4 class="m-0">
+              {{ song.songName }}
+            </h4>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </main>
 </template>
